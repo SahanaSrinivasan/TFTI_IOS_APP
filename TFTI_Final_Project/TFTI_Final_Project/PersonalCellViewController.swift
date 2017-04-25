3be0a5c853f462ad1abcb0994ed626af80fb756c
SahanaSrinivasan <sahanas@calvisitor-10-105-139-105.calvisitor.1918.berkeley.edu>
2017-04-24 20:17:18 -0700

    created all views

---
+++ TFTI_Final_Project/TFTI_Final_Project/PersonalCellViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PersonalCellViewController.swift
+//  TFTI_Final_Project
+//
+//  Created by Sahana Srinivasan on 4/24/17.
+//  Copyright © 2017 Sahana Srinivasan. All rights reserved.
+//
+
+import UIKit
+
+class PersonalCellViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
