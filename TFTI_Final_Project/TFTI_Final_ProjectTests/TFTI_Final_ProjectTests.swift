080d050785404ed39c0d46de0da8f882420c7d0c
Cindy Yuan <cyuan@berkeley.edu>
2017-05-02 23:51:47 -0700

    styling

--- TFTI_Final_Project/TFTI_Final_ProjectTests/TFTI_Final_ProjectTests.swift
+++ TFTI_Final_Project/TFTI_Final_ProjectTests/TFTI_Final_ProjectTests.swift
@@ -6,31 +6,30 @@
 //  Copyright © 2017 Sahana Srinivasan. All rights reserved.
 //
 
-import XCTest
-@testable import TFTI_Final_Project
+//import XCTest
+//@testable import TFTI_Final_Project
 
-class TFTI_Final_ProjectTests: XCTestCase {
+//class TFTI_Final_ProjectTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
+//    override func setUp() {
+//        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  //  }
     
-    override func tearDown() {
+//    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+//        super.tearDown()
+ //   }
     
-    func testExample() {
+//    func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
+ //   }
+ //   func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+ //       self.measure {
+  //          // Put the code you want to measure the time of here.
+ //       }
+//    }
     
-}
+//}
